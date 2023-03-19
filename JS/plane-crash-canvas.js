8ca5a8ea99aa22fcb608b1032a3e8755d27c54d5
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-03-18 23:07:13 -0700

    background

--- JS/plane-crash-canvas.js
+++ JS/plane-crash-canvas.js
@@ -3,4 +3,5 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = 1600;
     canvas.height = 900;
+    // canvas.style = "background: url('../Img/planecrash2.jpg')";
 })
