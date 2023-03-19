48e29873372d2a62202f6d8e89e4cead2832a54f
EmmieFish <105821167+EmmieFish@users.noreply.github.com>
2023-03-19 00:35:23 -0700

    broken

--- JS/plane-crash-canvas.js
+++ JS/plane-crash-canvas.js
@@ -1,6 +1,4 @@
 window.addEventListener('load', function(){
     const canvas = document.getElementById('planeCanvas');
     const ctx = canvas.getContext('2d');
-    canvas.width = 1600;
-    canvas.height = 900;
 })
